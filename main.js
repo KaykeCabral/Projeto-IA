d9b0d94f0712a6f2fc9e71b84412230b4a0c9afc
Kayke Cabral <125477411+KaykeCabral@users.noreply.github.com>
2024-12-01 20:18:18 -0300

    s

--- main.js
+++ main.js
@@ -32,9 +32,9 @@ const perguntas = [
         ]
         
     },
-    {
+{
         enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
-        alternativas: alternativas: [
+        alternativas: [
             {
                 texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
                 afirmacao: "afirmacao"
@@ -47,7 +47,7 @@ const perguntas = [
     },
     {
         enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
-        alternativas: alternativas: [
+        alternativas: [
             {
                 texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
                 afirmacao: "afirmacao"
@@ -60,7 +60,7 @@ const perguntas = [
     },
     {
         enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
-        alternativas: alternativas: [
+        alternativas: [
             {
                 texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
                 afirmacao: "afirmacao"
