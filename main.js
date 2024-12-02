20b3a1694ac7ec9374f6b02e39d7d5cdbb75f26b
Kayke Cabral <125477411+KaykeCabral@users.noreply.github.com>
2024-12-01 21:11:34 -0300

    s

--- main.js
+++ main.js
@@ -19,28 +19,28 @@ const perguntas = [
         ]
 },
 {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Dentro da caverna, Aric encontra uma encruzilhada. Ele decide:",
         alternativas: [
             {
-                texto:  "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+                texto:  "Tomar o caminho iluminado por cristais",
                 afirmacao: "afirmacao"
             },
             {
-                texto:  "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                texto:  "Seguir o caminho coberto por teias de aranha",
                 afirmacao: "afirmacao"
             }
         ]
         
     },
 {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Com novas pistas, Aric chega a uma fortaleza demoníaca. Ele opta por:",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                texto: "Invadir furtivamente para coletar informações",
                 afirmacao: "afirmacao"
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                texto: "Confrontar os demônios diretamente",
                 afirmacao: "afirmacao"
             }
         ]
